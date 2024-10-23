41c47179c3835e035e50dd98e24da8f83942df93
Abdul1091 <Abdul2kur1@gmail.com>
2024-10-23 23:17:15 +0000

    Implement web cache with tracking and 10s expiration

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,22 +13,18 @@ redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks the request count.
+    '''Caches the output of fetched data.
     '''
     @wraps(method)
     def invoker(url) -> str:
-        '''The wrapper function for caching the output and tracking the count.
+        '''The wrapper function for caching the output.
         '''
-        # Increment access count for the URL
         redis_store.incr(f'count:{url}')
-        
-        # Check if the result is already cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        
-        # If not cached, fetch the result and cache it for 10 seconds
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
@@ -36,6 +32,7 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL, caches the result, and tracks the access count.
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
     '''
     return requests.get(url).text
